7a222576c8c5ed97ca12345faeeda509a7b9c48d
Kelvin~M. <mwaniki.kelvin918@gmail.com>
2024-05-26 20:26:19 +0300

    Initial commit of the Git module as a part of the Python learning stack

---
+++ kelvin-git-learning/test.py
@@ -0,0 +1,6 @@
+# Initializes a list containing names of different fruits and assigns the list to a variable named fruits 
+fruits = ['apple', 'pear', 'orange', 'appricot']
+
+# Iterates through the various items in the list, printing each item in capital letters as it is iterrated
+for fruit in fruits:
+    print(fruit.capitalize())
