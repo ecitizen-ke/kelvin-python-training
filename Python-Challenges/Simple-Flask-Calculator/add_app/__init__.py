8f442bf1cb8d1609685b872a713d71ebbeeb124d
Kelvin~M <101367388+akaTheJew@users.noreply.github.com>
2024-06-02 14:56:06 +0300

    Revert "Initial commit for the add feature"

--- Python-Challenges/Simple-Flask-Calculator/add_app/__init__.py
+++
@@ -1,4 +0,0 @@
-# Initializing a the Blueprint dependency from the Flask module
-from flask import Blueprint
-# Assigning the add blueprint to a variable
-add_bp = Blueprint('add', __name__)
--- Python-Challenges/Simple-Flask-Calculator/add_app/add.py
+++
@@ -1,3 +0,0 @@
-# Defining an add functiont to add two numbers together
-def add(num1, num2):
-    return num1 + num2
